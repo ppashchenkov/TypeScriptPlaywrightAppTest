7fa6de2c9fd90cae23ff3b768f6623f3e6a3f838
ppashchenkov <email@example.com>
2025-01-21 16:53:56 +0300

    add reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 60 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in await expect(locator).toHaveText();
+     */
+    timeout: 5000
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,11 +30,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['json', {  outputFile: 'report/json-report/report.json' }],
+    ['html', { outputFolder: 'reports/html-report/', open: 'never' }],
+    ['junit', { outputFile: 'report/junit-report/report.xml' }],
+    ['@estruyf/github-actions-reporter'],
+    ['monocart-reporter', {
+      name: "My Test Report",
+      outputFile: './monocart-report/index.html'
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:5000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/navigationUi.spec.ts
+++ tests/navigationUi.spec.ts
@@ -7,20 +7,18 @@ import {test, expect}  from "@playwright/test"
     test.describe('Navigation tab are available', async () => {
 
         test.beforeEach('navigate to home page', async({ page }) => {
-            await page.goto('http://localhost:5000')
-            test.setTimeout(5000)
+            await page.goto('/')
         })
 
         test( `Test Case 1: Verify ${tabName} tab should be available`, async ({ page}) => {
-            const tab = await page.getByRole('link',
-                {name: `${tabName}`, exact: true})
+            const tab = page.getByRole('link', {name: `${tabName}`, exact: true})
             const tabClassAttribute = await tab.getAttribute('class')
 
             await expect(tab).toBeAttached()
             await expect(tab).toHaveCount(1)
             await expect(tab).toBeVisible()
             await expect(tab).toBeEnabled()
-            await expect(tabClassAttribute).toStrictEqual(`${expectedClass}`)
+            expect(tabClassAttribute).toStrictEqual(`${expectedClass}`)
         })
     })
 })
