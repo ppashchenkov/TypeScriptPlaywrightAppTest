07c4b8af5e05db02581f7c40f88371c7fd00962c
ppashchenkov <email@example.com>
2025-01-27 15:10:32 +0300

    Refactoring run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,4 @@ rm -rf allure-results
 npx playwright test
 cp -a reports/allure-report/history allure-results/
 npx allure generate allure-results --report-dir reports/allure-report --clean
+git restore reports/
