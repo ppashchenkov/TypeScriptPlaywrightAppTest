6899386b7981e57b58905e0036cfe346612543cf
ppashchenkov <email@example.com>
2025-02-04 14:16:39 +0300

    Try 8 repeates run test search.func.spec.ts

--- tests/search.func.spec.ts
+++ tests/search.func.spec.ts
@@ -4,29 +4,44 @@ import {HomePage} from "../pages/home.page";
 import { SearchPage } from "../pages/search.page";
 import { users } from "../testData/usersTestData";
 
-test.describe('Should Search Users By Search Criteria', async() => {
+[
+    {tcRunNumber: "Run_01" },
+    {tcRunNumber: "Run_02" },
+    {tcRunNumber: "Run_03" },
+    {tcRunNumber: "Run_04" },
+    {tcRunNumber: "Run_05" },
+    {tcRunNumber: "Run_06" },
+    {tcRunNumber: "Run_07" },
+    {tcRunNumber: "Run_08" }
+].forEach(({tcRunNumber}) => {
+    test.describe('Should Search Users By Search Criteria', async() => {
     let apiRequest: APIRequestContext;
 
-    test.beforeEach('Create API Request Context, Create Preconditions', async({ page }) => {
-        apiRequest = await request.newContext();
-        await preconditions.deleteAllUsers(apiRequest);
-        await preconditions.createUsers(apiRequest)
+        test.beforeEach('Create API Request Context, Create Preconditions', async({ page }) => {
+            apiRequest = await request.newContext();
+            await preconditions.deleteAllUsers(apiRequest);
+            await preconditions.createUsers(apiRequest)
 
-        await page.goto('/');
-    })
+            await page.goto('/');
+        })
 
-    test('Search User With Unique First Name', async({ page }) => {
-        const userJohn = users.user1;
-        await new HomePage(page)
-            .clickSearchTab();
-        const searchPage = new SearchPage(page);
-        await searchPage.inputFirstName(userJohn.firstName);
-        await searchPage.clickSearchButton();
+        test(`Search User With Unique First Name: ${tcRunNumber}`, async({ page }) => {
+            const userJohn = users.user1;
+            await new HomePage(page)
+                .clickSearchTab();
+            const searchPage = new SearchPage(page);
+            await searchPage.inputFirstName(userJohn.firstName).then((res) => {
+                return res;
+            }).catch((err) => {
+                return err;
+            });
+            await searchPage.clickSearchButton();
 
-        expect(await searchPage.getTbodyRowCounts()).toBe(1);
-    })
+            expect(await searchPage.getTbodyRowCounts()).toBe(1);
+        })
 
-    test.afterEach('Close API request context', async({ page }) => {
-        await apiRequest.dispose();
+        test.afterEach('Close API request context', async({ page }) => {
+            await apiRequest.dispose();
+        })
     })
 })
