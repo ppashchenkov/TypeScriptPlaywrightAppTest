ed664931499a8ccdaf7e09718423844cb3724723
ppashchenkov <ppashchenkov@gmail.com>
2025-02-19 08:03:35 +0300

    refactoring playwright.config.ts: workers: process.env.CI ? 1 : 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 2,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   reporter: [
     ['list'],
     ['json', {  outputFile: 'reports/json-report/report.json' }],
